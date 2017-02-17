aea2485265e6280fe3e8d2b8b4dca000d551b458
happy240 <happy78@live.com>
2017-02-17 18:10:01 +0800

    Pomoapikey Setting

--- js/settings.js
+++ js/settings.js
@@ -3,20 +3,15 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
-var fruitSelector = document.getElementById('fruit');
-var vegetableSelector = document.getElementById('vegetable');
+var pomoapikey = document.getElementById('pomoapikey');
 
 t.render(function(){
   return Promise.all([
-    t.get('board', 'shared', 'fruit'),
-    t.get('board', 'private', 'vegetable')
+    t.get('board', 'shared', 'pomoapikey')
   ])
-  .spread(function(savedFruit, savedVegetable){
-    if(savedFruit && /[a-z]+/.test(savedFruit)){
-      fruitSelector.value = savedFruit;
-    }
-    if(savedVegetable && /[a-z]+/.test(savedVegetable)){
-      vegetableSelector.value = savedVegetable;
+  .spread(function(savedPomoapikey){
+    if(savedPomoapikey){
+      pomoapikey.value = savedPomoapikey;
     }
   })
   .then(function(){
@@ -26,10 +21,7 @@ t.render(function(){
 });
 
 document.getElementById('save').addEventListener('click', function(){
-  return t.set('board', 'private', 'vegetable', vegetableSelector.value)
-  .then(function(){
-    return t.set('board', 'shared', 'fruit', fruitSelector.value);
-  })
+  return t.set('board', 'private', 'pomoapikey', pomoapikey.value)
   .then(function(){
     t.closePopup();
   })
